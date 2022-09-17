9fc4cde304567e563bec171ebfb0bd53c6351f95
Bharadwaj10000 <br722616@gmail.com>
2022-09-17 06:40:20 +0530

    updated

---
+++ Javascript/eventbubbling.js
@@ -0,0 +1,7 @@
+document.getElementById("child").addEventListener("click",function(){
+    alert("You clicked the child element");
+},false);
+
+document.getElementById("parent").addEventListener("click",function(){
+    alert("You clicked the parent element");
+},false);
