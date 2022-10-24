1a5913ba50b1afecab6003b2f78d8099d8160954
Bharadwaj10000 <br722616@gmail.com>
2022-10-24 11:35:41 +0530

    completed

---
+++ Digital_Clock_useEffect()/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import './DigiClock';
+import DigiClock from "./DigiClock";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DigiClock/>
+    </div>
+  );
+}
