c79767818f5d8fa77e8c4f8ce5d6bc92719bcedc
Bharadwaj10000 <br722616@gmail.com>
2022-10-24 12:57:19 +0530

    updated

---
+++ Array_in_table_form/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./DisplayData";
+import DisplayData from "./DisplayData";
+function App() {
+  return (
+    <div className="App">
+      <DisplayData />
+    </div>
+  );
+}
+export default App;
