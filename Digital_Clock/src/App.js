40f4ba65fa43ee9b0080f584da6ff5e89ef028ca
Bharadwaj10000 <br722616@gmail.com>
2022-10-24 12:41:06 +0530

    updated

---
+++ Digital_Clock/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import './DigitalClock';
+import DigitalClock from "./DigitalClock";
+function App() {
+  return (
+    <div className="App" style={{width:'90%', margin: '0 auto'}}>
+      <DigitalClock/>
+    </div>
+  );
+}
+
+export default App;
