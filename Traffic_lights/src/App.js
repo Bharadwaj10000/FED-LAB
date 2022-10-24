0bb182a17b8aae064a9c3a3849721e8d918fe2ea
Bharadwaj10000 <br722616@gmail.com>
2022-10-24 12:48:23 +0530

    updated

---
+++ Traffic_lights/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import "./ChangeColors";
+import ChangeColors from "./ChangeColors";
+export default function App() {
+  return (
+    <div className="App">
+      <ChangeColors />
+    </div>
+  );
+}
