f31dd3605adaaf4221d56ed8fa43a5ff7078d047
Bharadwaj10000 <br722616@gmail.com>
2022-11-06 19:58:48 +0530

    updated

---
+++ higher-order-functions/src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import HigherOrderFunctions from "./HigherOrderFunctions";
+export default function App() {
+  return (
+    <div className="App">
+      <HigherOrderFunctions />
+    </div>
+  );
+}
